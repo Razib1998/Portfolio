82efb03d12fb62b60c5a0bcf4c0e68355c0f833c
Md   Razib Sarker <mdrajibsorkar965@gmail.com>
2025-07-14 20:50:55 +0200

    update package.json file fro deploy

--- src/Components/Social.tsx
+++ src/Components/Social.tsx
@@ -1,16 +1,39 @@
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconBrandTwitter,
+} from "@tabler/icons-react";
 
-import { socialLinks } from "../User";
+const Social = () => {
+  const socialLinks = [
+    { link: "https://github.com/Razib1998", icon: IconBrandGithub },
+    {
+      link: "https://www.linkedin.com/in/md-razib-sarker-5550a2300",
+      icon: IconBrandLinkedin,
+    },
 
-const Social=()=>{
-    const socialIcons=socialLinks.map((socialLink, index)=>{
-        return <a   key={index} href={`${socialLink.link}`} target="_blank"  className="font-mono text-lg  hover:text-primaryColor hover:-translate-x-1 transition transform duration-300 ease-in-out">
-       <div data-aos="fade-up-left" data-aos-duration="800" > <socialLink.icon stroke={1.5} className="-rotate-90" size={25} /></div>
-    </a>
-    })
+    { link: "https://x.com/mdsarker143?s=21", icon: IconBrandTwitter },
+  ];
+  const socialIcons = socialLinks.map((socialLink, index) => {
+    return (
+      <a
+        key={index}
+        href={`${socialLink.link}`}
+        target="_blank"
+        className="text-lg  hover:text-primaryColor hover:-translate-x-1 transition transform duration-300 ease-in-out"
+      >
+        <div>
+          <socialLink.icon stroke={1.5} className="-rotate-90" size={25} />
+        </div>
+      </a>
+    );
+  });
 
-    return <div  className="flex md-mx:hidden text-textColor items-center gap-8 fixed bottom-32 -left-48 rotate-90 " >
-        {socialIcons}
-        <hr className="border w-40 rounded-full  bg-textColor border-textColor"/>
+  return (
+    <div className="flex !md:hidden text-textColor items-center gap-8 fixed bottom-32 -left-32 rotate-90">
+      {socialIcons}
+      <hr className="border w-40 rounded-full  bg-textColor border-textColor" />
     </div>
-}
+  );
+};
 export default Social;
--- src/User.tsx
+++ src/User.tsx
@@ -1,150 +1,176 @@
-import { IconBrandGithub, IconBrandInstagram, IconBrandLeetcode, IconBrandLinkedin, IconBrandYoutube } from "@tabler/icons-react";
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconBrandTwitter,
+} from "@tabler/icons-react";
+import { title } from "process";
 const Info = {
-    name: "Chandrabhan Maurya",
-    stack: ["Software Engineer", "Full Stack Developer", "Competitive Programmer", "Freelancer", "Open Source Contributor"],
-    bio: "I'm a passionate software engineer specialized in crafting efficient and scalable solutions, deliver top-notch web applications, and provide freelance services that exceed client expectations. Let's connect and create something amazing together!"
-}
-
-
+  name: "Md Razib Sarker",
+  stack: ["Full Stack Developer", "Competitive Programmer"],
+  bio: "I'm a passionate Full Stack Web Developer specialized in building efficient, scalable, and high-quality web applications. I focus on delivering seamless end-to-end solutions and always aim to create impactful, user-friendly digital experiences. Let's connect and build something amazing together!",
+};
 
 const ProjectInfo = [
-    {
-        title: "Ekart E-comm App",
-        desc: "Ekart is a modern, fully responsive e-commerce platform built using React, TailwindCSS, Heroicons, and HeadlessUI, offering a seamless shopping experience across all devices. It features secure user authentication with login, registration, and logout functionalities. Users can add items to a wishlist or remove them, search for specific products, and add them to a shopping cart for purchase. The checkout process includes form validation for address and payment information, ensuring accuracy and security. After purchase, users can view their order history in the Orders section. Ekart combines robust functionality with a sleek, intuitive design, making online shopping effortless and enjoyable.",
-        image: "Ekart.png",
-        live: true,
-        technologies: ["React", "Tailwind", "Redux", "Heroicons", "HeadlessUI"],
-        link: "https://code-mars.github.io/Ekart/",
-        github: "https://github.com/Code-Mars/Ekart/"
-    },
-    {
-        title: "Facebook Clone",
-        desc: "Facebook Clone is a full-stack web application that replicates the core features of Facebook, including user authentication, posting, and commenting. The front end is built with React, TailwinCSS, Tabler-Icons, Mantine and Firebase, while the back end uses Firebase for user authentication and data storage. Users can create an account, log in, post updates, and comment on posts. The app features real-time updates, so users can see new comments as they are posted. Facebook Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-        image: "Facebook.png",
-        live: false,
-        technologies: ["React", "Springboot", "MongoDB", "Tailwind", "Tabler-Icons", "Firebase"],
-        link: "https://github.com/Code-Mars/Facebook-Clone",
-        github: "https://github.com/Code-Mars/Facebook-Clone"
-    },
-    {
-        title: "Spotify Clone",
-        desc: "Spotify Clone is a full-stack web application that replicates the core features of Spotify, including user authentication, music playback, and playlist creation. The front end is built with React, TailwindCSS, Heroicons, and Firebase, while the back end uses Firebase for user authentication and data storage. Users can create an account, log in, search for songs, and create playlists. The app features real-time updates, so users can see new songs as they are added. Spotify Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-        image: "Spotify.png",
-        live: false,
-        technologies: ["React", "Springboot", "Tailwind", "Tabler-Icons", "MongoDB", "Firebase"],
-        link: "https://github.com/Code-Mars/Spotify-Clone",
-        github: "https://github.com/Code-Mars/Spotify-Clone"
-    },
-    {
-        title: "Travel Tracker",
-        desc: "Travel Tracker is a full-stack web application that allows users to track their travel experiences and share them with others. The front end is built with React, Bootstrap, while the back end uses Node.js, Express, and PostgresSQL. Users can create an account, log in, add new trips, and view their past trips on a map. The app features a clean, modern design with interactive maps and a user-friendly interface. Travel Tracker combines functionality with style, making it easy and enjoyable to record and share travel memories.",
-        image: "Travel.png",
-        live: false,
-        technologies: ["React", "Node.js", "PostgresSQL", "Express", "Bootstrap"],
-        link: "https://github.com/Code-Mars/Travel-Tracker",
-        github: "https://github.com/Code-Mars/Travel-Tracker"
-    },
-    {
-        title: "Instagram Clone",
-        desc: "Instagram Clone is a full-stack web application that replicates the core features of Instagram, including user authentication, image uploading, and commenting. The front end is built with React, TailwindCSS, and Heroicons, while the back end uses Firebase for user authentication and image storage. Users can create an account, log in, upload images, and comment on posts. The app features real-time updates, so users can see new comments as they are posted. Instagram Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-        image: "Instagram.png",
-        live: false,
-        technologies: ["React", "Springboot", "Tailwind", "Heroicons", "Firebase", "MongoDB"],
-        link: "https://github.com/Code-Mars/Instagram-Clone",
-        github: "https://github.com/Code-Mars/Instagram-Clone"
-    },
-    {
-        title: "CodeX Code Editor",
-        desc: "CodeX is a modern, fully responsive code editor built using React, TailwindCSS, and Ace Editor, offering a seamless coding experience across all devices. It features syntax highlighting for popular programming languages, including HTML, CSS, JavaScript, Python, and Java. Users can create, edit, and save code files, with the option to download them as text files. CodeX combines robust functionality with a sleek, intuitive design, making coding effortless and enjoyable.",
-        image: "CodeX.png",
-        live: false,
-        technologies: ["React", "Tailwind", "Ace Editor"],
-        link: "https://github.com/Code-Mars/CodeX",
-        github: "https://github.com/Code-Mars/CodeX"
-    }
-]
+  {
+    title: "Vdemy",
+    desc: `Vdemy is a modern, fully responsive online course-selling platform built     using the latest web technologies. Designed for both instructors and students,   Vdemy offers a seamless learning and teaching experience across all devices.
+       The platform features secure user authentication with login, registration, and logout functionalities. Anyone can register as an instructor and create courses consisting of multiple modules, with each module containing multiple lessons. Once a course is published, it becomes available for students to browse and purchase. Students can register, buy courses, and access structured lessons within modules. The platform ensures smooth course progression and provides certificates of completion upon successfully finishing a course, signed by the respective instructor.
+       Vdemy combines powerful functionality with a sleek, intuitive interface, making it easy to teach, learn, and grow—anytime, anywhere.
+`,
+    image: "https://i.ibb.co/5ggbFPjc/Screenshot-2025-07-14-at-16-55-11.png",
+    live: true,
+    technologies: [
+      "Next Js",
+      "Next Auth",
+      "Next Cloudinary",
+      "Strip",
+      "Mongodb",
+      "Tailwind",
+      "Nodemailer",
+      "Schadcn",
+    ],
+    link: "https://educonnect-five-delta.vercel.app",
+    github: "https://github.com/Razib1998/educonnect",
+  },
+  {
+    title: "StayVista",
+    desc: `StayVista is a modern, fully responsive accommodation booking platform inspired by Airbnb, designed with a robust three-layer architecture for guests, hosts, and admins. Built for seamless usability across all devices, StayVista delivers a smooth and secure experience for both travelers and property owners.
 
+Users can register as guests, and upon admin approval, they may upgrade to hosts and list their rooms for reservation. Guests can browse available rooms, view host details, check availability, and book accommodations through secure card payments. Bookings can be managed easily, with options to edit or cancel reservations as needed.
+
+Admins have full control over the platform. They can manage user approvals, block users if necessary, and access comprehensive booking statistics for monitoring platform activity and performance.
+
+StayVista combines powerful features with a sleek, intuitive interface—making it easy to list, discover, and reserve stays with confidence.`,
+    image: "https://i.ibb.co/fzX1jp39/Screenshot-2025-07-14-at-16-55-38.png",
+    live: false,
+    technologies: [
+      "React",
+      "MongoDB",
+      "Stripe",
+      "Tailwind",
+      "Headless Ui",
+      "Firebase",
+    ],
+    link: "https://stayvista-flax.vercel.app",
+    github: "https://github.com/Razib1998/STAY-VISTA",
+  },
+  {
+    title: "Careear Hub",
+    desc: `CareerHub is a modern, fully responsive job portal application designed to streamline the job search and application process. Built for flexibility and ease of use, CareerHub empowers users to discover job opportunities and manage their applications efficiently across all devices.
+
+Users can browse detailed job listings and apply directly through the platform. After submitting an application, they can view the job details, edit their application if needed, or cancel it at any time—giving them full control over their job search journey.
+
+With its intuitive interface and user-friendly features, CareerHub makes it easier than ever for job seekers to find and apply for positions that match their skills and interests.`,
+    image: "https://i.ibb.co/Y40hZpJD/Screenshot-2025-07-14-at-17-55-33.png",
+    live: false,
+    technologies: ["React", "Firebase", "Tailwind", "MongoDB"],
+    link: "https://github.com/Code-Mars/Spotify-Clone",
+    github: "https://github.com/Code-Mars/Spotify-Clone",
+  },
+];
 
 const SkillInfo = [
-    {
-        title: "Frontend",
-        skills: ["HTML", "CSS", "SASS", "JavaScript", "React JS", "Angular", "Redux", "Tailwind CSS", "GSAP", "Material UI", "Bootstrap"
-        ]
-    },
-    {
-        title: "Backend",
-        skills: ["Springboot", "Node JS", "Express JS", "MySQL", "MongoDB", "Firebase", "PostgresSQL"]
-    },
-    {
-        title: "Languages",
-        skills: ["C", "C++", "Java", "JavaScript", "TypeScript"]
-    },
-    {
-        title: "Tools",
-        skills: ["Git", "Github", "VS Code", "Postman", "MongoDB Compass", "Spring Tool Suite"]
-    }
-]
-const socialLinks = [
-    { link: "https://github.com/Code-Mars", icon: IconBrandGithub },
-    { link: "https://www.linkedin.com/in/chandrabhan-maurya", icon: IconBrandLinkedin },
-    { link: "https://www.instagram.com/code.marshal_", icon: IconBrandInstagram }, 
-    { link: "https://www.youtube.com/channel/UC1ki6jaFvFiH_E79b9FYptw", icon: IconBrandYoutube },
-    { link: "https://www.leetcode.com/u/CodeMars", icon: IconBrandLeetcode }
+  {
+    title: "Frontend",
+    skills: [
+      {
+        title: "HTML",
+        icon: "https://i.ibb.co/bMLWj8jm/HTML.png",
+      },
+      { title: "CSS", icon: "https://i.ibb.co/hvwhQQn/CSS.png" },
+      {
+        title: "JavaScript",
+        icon: "https://i.ibb.co/cStPX9K7/Java-Script.png",
+      },
+      { title: "React JS", icon: "https://i.ibb.co/HDSpzwX1/React-JS.png" },
+      { title: "Next JS", icon: "https://i.ibb.co/qY3Jt7qG/next-js.png" },
+      { title: "Redux", icon: "https://i.ibb.co/M5BVK4cN/Redux.png" },
+      { title: "Tailwind CSS", icon: "https://i.ibb.co/KpmTM8wN/tailwind.png" },
+      {
+        title: "Material UI",
+        icon: "https://i.ibb.co/MkhY55Vc/Material-UI.png",
+      },
+      { title: "GraphQL", icon: "https://i.ibb.co/d0z3T5Yx/graphnql.png" },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      { title: "Node JS", icon: "https://i.ibb.co/WTbhG1B/Node-JS.png" },
+      { title: "Express JS", icon: "https://i.ibb.co/CpDHGFvx/Express-JS.png" },
+      { title: "Prisma ORM", icon: "https://i.ibb.co/Jw1304Dr/prisma.jpg" },
+      { title: "MySQL", icon: "https://i.ibb.co/Ndj64Dhd/MySQL.png" },
+      { title: "MongoDB", icon: "https://i.ibb.co/Ldw2sD1Z/MongoDB.png" },
+      { title: "Firebase", icon: "https://i.ibb.co/B2Ng4JC7/Firebase.png" },
+      {
+        title: "PostgresSQL",
+        icon: "https://i.ibb.co/twdHhpsv/Postgres-SQL.png",
+      },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      { title: "C++", icon: "https://i.ibb.co/HDJyZXjB/C.png" },
+      { title: "Python", icon: "https://i.ibb.co/dJxKFJmS/python.png" },
+      {
+        title: "JavaScript",
+        icon: "https://i.ibb.co/cStPX9K7/Java-Script.png",
+      },
+      { title: "TypeScript", icon: "https://i.ibb.co/vn1GQ7N/Type-Script.png" },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      { title: "Git", icon: "https://i.ibb.co/DDpSH3wk/Git.png" },
+      { title: "Github", icon: "https://i.ibb.co/HD6FFYrW/Github.png" },
+      { title: "VS Code", icon: "https://i.ibb.co/gZHY6BnN/VS-Code.png" },
+      { title: "Postman", icon: "https://i.ibb.co/hFLwL9Cs/Postman.png" },
+      {
+        title: "MongoDB Compass",
+        icon: "https://i.ibb.co/wFRjv6V5/Mongo-DB-Compass.png",
+      },
+    ],
+  },
 ];
-
+const socialLinks = [
+  { link: "https://github.com/Razib1998", icon: IconBrandGithub },
+  {
+    link: "https://www.linkedin.com/in/md-razib-sarker-5550a2300",
+    icon: IconBrandLinkedin,
+  },
+  { link: "https://x.com/mdsarker143?s=21", icon: IconBrandTwitter },
+];
 
 const ExperienceInfo = [
-    {
-        role: "Specialist Programmer",
-        company: "Infosys",
-        date: "Oct 2023 - Present",
-        desc: "I led software development with Spring Boot, React, and Angular, creating scalable microservices and interfaces. I optimized performance by integrating frontend and backend, and enhanced security with RESTful APIs. Collaborating in agile teams, I fostered innovation and efficiency for top-quality solutions.",
-        skills: ["Springboot", "React JS", "Angular", "Node JS", "MySQL", "MongoDB", "Microservices"]
-    },
-    {
-        role: "System Engineer",
-        company: "Infosys",
-        date: "Nov 2022 - Sep 2023",
-        desc: " I leveraged my skills in Java, Selenium, automation testing, and MySQL to design and implement automated testing frameworks, ensuring robust software validation. I optimized database performance and reliability, and collaborated with cross-functional teams to enhance system operations and ensure seamless integration.",
-        skills: ["Java", "MySQL", "Hibernate", "Selenium", "Jenkins", "JIRA", "Automation Testing"]
-    }
-]
-const Slugs = [
-    "typescript",
-    "spring",
-    "javascript",
-    "dart",
-    "java",
-    "react",
-    "angular",
-    "flutter",
-    "android",
-    "html5",
-    "css3",
-    "springboot",
-    "mongodb",
-    "selenium",
-    "nodedotjs",
-    "express",
-    "nextdotjs",
-    "prisma",
-    "mysql",
-    "amazonaws",
-    "postgresql",
-    "firebase",
-    "nginx",
-    "vercel",
-    "testinglibrary",
-    "jest",
-    "cypress",
-    "docker",
-    "git",
-    "jira",
-    "github",
-    "gitlab",
-    "visualstudiocode",
-    "androidstudio",
-    "sonarqube",
-    "figma",
+  {
+    role: "Web Instructor",
+    company: "Programming Hero",
+    date: "Oct 2023 - March 2025",
+    desc: "As a web instructor at Programming Hero, my key responsibilities include guiding students through web development concepts, helping them debug code, and reviewing their assignments. I support students by answering questions, encouraging best practices, and providing clear, constructive feedback. I also stay engaged through mentorship, live sessions, and timely communication to ensure a smooth and effective learning experience.",
+    skills: ["React JS", "Next JS", "Node JS", "MySQL", "MongoDB", "Redux"],
+  },
 ];
-export { Info, ProjectInfo,socialLinks, SkillInfo, ExperienceInfo, Slugs };
+const Slugs = [
+  "typescript",
+  "javascript",
+  "react",
+  "html5",
+  "css3",
+  "mongodb",
+  "express",
+  "prisma",
+  "mysql",
+  "amazonaws",
+  "socket.io",
+  "postgresql",
+  "firebase",
+  "vercel",
+  "docker",
+  "git",
+  "github",
+  "visualstudiocode",
+  "figma",
+];
+export { Info, ProjectInfo, socialLinks, SkillInfo, ExperienceInfo, Slugs };
